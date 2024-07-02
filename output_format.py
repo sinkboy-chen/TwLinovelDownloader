8b516e0224d4ecaddc51740ffcf7810974788333
sinkboy-chen <129042729+sinkboy-chen@users.noreply.github.com>
2024-07-02 23:13:35 +0800

    test calibre ebook-convert tool before use

--- output_format.py
+++ output_format.py
@@ -48,10 +48,28 @@ def convert_format(original_path, target_format):
     output_path = path.parent / path.stem
     output_path = output_path.with_suffix(f".{target_format}")
 
+    CREATE_NO_WINDOW = 0x08000000
+    print(f"正在測試轉檔工具......")
+    try:
+        subprocess.run(
+            ["ebook-convert"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+            creationflags=CREATE_NO_WINDOW
+        )
+        print(f"轉檔工具測試成功")
+    except:
+        print("轉檔工具測試失敗")
+        print("可能")
+        print("1.未安裝calibre [https://calibre-ebook.com/download]")
+        print("2.calibre ebook-convert.exe 的資料夾不在環境變數中")
+        print("若無法解決此問題 輸出檔案格式選擇 epub")
+        return 0, None
+
     print(f"正在轉成{target_format}檔案格式......")
     
     try:
-        CREATE_NO_WINDOW = 0x08000000
+        
         subprocess.run(
             ["ebook-convert", original_path, output_path], 
             stdout=subprocess.DEVNULL,
